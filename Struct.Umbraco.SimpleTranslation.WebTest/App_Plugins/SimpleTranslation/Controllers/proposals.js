b46b6e89d1312291cf249fd65b513336fba4e26c
RasiahT <rasiahmt@gmail.com>
2017-02-16 16:46:53 +0100

    Added angular files

---
+++ Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/Controllers/proposals.js
@@ -0,0 +1,5 @@
+﻿angular.module("umbraco").controller("SimpleTranslation.Proposals.Controller", function () {
+
+    
+
+});
---
+++ Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/Controllers/tasks.js
@@ -0,0 +1,5 @@
+﻿angular.module("umbraco").controller("SimpleTranslation.Tasks.Controller", function () {
+
+    
+
+});
