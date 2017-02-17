d2951d63dcd41570c2dd9513eee734c574cf15d5
LuqJensen <lucas__jensen@hotmail.com>
2017-02-17 14:03:06 +0100

    Initial work on PairsController for listing translatable strings [U11]...
    TODO: get rid of dynamic object usage. Determine how to use umbraco petapoco or an existing model instead.

---
+++ Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/Controllers/Api/PairsController.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web;
+using System.Web.Http;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Services;
+using Umbraco.Web;
+using Umbraco.Web.WebApi;
+
+namespace Struct.Umbraco.SimpleTranslation.WebTest.App_Plugins.SimpleTranslation.Controllers.Api
+{
+    public class PairsController : UmbracoAuthorizedApiController
+    {
+        [HttpGet]
+        public object GetTranslatableKeys()
+        {
+            var db = DatabaseContext.Database;
+            var results = db.Fetch<dynamic>(new Sql().Select("*").From("dbo.cmsDictionary"));
+            var subNodes = results.Where(x => x.parent != null).ToLookup(x => x.parent, x => x);
+            var rootNodes = results.Where(x => x.parent == null);
+            dynamic result = new
+            {
+                children = new Dictionary<dynamic, dynamic>()
+            };
+
+            foreach (var v in rootNodes)
+            {
+                result.children.Add(v.id, v);
+                BuildDictionary(v, subNodes);
+            }
+            return result;
+        }
+
+        private void BuildDictionary(dynamic currentNode, ILookup<dynamic, dynamic> subNodes)
+        {
+            foreach (var child in subNodes[currentNode.id])
+            {
+                // Weird issue with .Count and .Any() on IGrouping<dynamic, dynamic>. So iterate once and break instead.
+                currentNode.children = new Dictionary<dynamic, dynamic>();
+                foreach (var v in subNodes[currentNode.id])
+                {
+                    currentNode.children.Add(v.id, v);
+                    BuildDictionary(v, subNodes);
+                }
+                break;
+            }
+        }
+    }
+}
--- Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/Controllers/pairs.js
+++ Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/Controllers/pairs.js
@@ -1,5 +1,6 @@
-﻿angular.module("umbraco").controller("SimpleTranslation.Pairs.Controller", function () {
-
-    
+﻿angular.module("umbraco").controller("SimpleTranslation.Pairs.Controller", function($scope, $http) {
+    $http.get('/umbraco/backoffice/api/Pairs/GetTranslatableKeys').success(function(response) {
+        console.log(response);
+    });
 
 });
