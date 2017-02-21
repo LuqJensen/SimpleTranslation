9a8860981fc553df84940ceb9583642a1ec84f7f
LuqJensen <lucas__jensen@hotmail.com>
2017-02-21 01:08:23 +0100

    Added the ability to reject proposals...
    Added dialog boxes for confirmation upon rejecting a proposal or accepting a proposal with multiple available proposals for the same key and language.
    Added Bootbox.js, a library for easy dialog boxes when coupled with bootstrap and jquery.
    TODO: error handling, styling improvements and separation of concerns.

--- Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/Controllers/Api/ProposalsController.cs
+++ Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/Controllers/Api/ProposalsController.cs
@@ -20,8 +20,8 @@ namespace Struct.Umbraco.SimpleTranslation.WebTest.App_Plugins.SimpleTranslation
         {
             var db = DatabaseContext.Database;
             var proposals = db.Fetch<TranslationProposal>(new Sql()
-                    .Select("p.*, u.userName as userName, l.languageCultureName as language")
-                    .From("dbo.simpleTranslationProposals p LEFT JOIN dbo.umbracoLanguage l on p.languageId=l.id LEFT JOIN dbo.umbracoUser u on u.id=p.userId"))
+                    .Select("p.*, u.userName AS userName, l.languageCultureName AS language")
+                    .From("dbo.simpleTranslationProposals p LEFT OUTER JOIN dbo.umbracoLanguage l ON p.languageId=l.id LEFT OUTER JOIN dbo.umbracoUser u ON u.id=p.userId"))
                 .ToLookup(x => x.UniqueId, x => x);
             if (!proposals.Any())
             {
@@ -43,11 +43,11 @@ namespace Struct.Umbraco.SimpleTranslation.WebTest.App_Plugins.SimpleTranslation
         public void AcceptProposal(int id)
         {
             var db = DatabaseContext.Database;
-            var proposal = db.FirstOrDefault<TranslationProposal>(new Sql("select * from dbo.simpleTranslationProposals where pk=@tag", new
+            var proposal = db.FirstOrDefault<TranslationProposal>(new Sql("SELECT * FROM dbo.simpleTranslationProposals WHERE pk=@tag", new
             {
                 tag = id
             }));
-            var existingData = db.FirstOrDefault<TranslationText>(new Sql("select * from dbo.cmsLanguageText where UniqueId=@tag1 and languageId=@tag2", new
+            var existingData = db.FirstOrDefault<TranslationText>(new Sql("SELECT * FROM dbo.cmsLanguageText WHERE UniqueId=@tag1 AND languageId=@tag2", new
             {
                 tag1 = proposal.UniqueId,
                 tag2 = proposal.LanguageId
@@ -69,10 +69,20 @@ namespace Struct.Umbraco.SimpleTranslation.WebTest.App_Plugins.SimpleTranslation
                 db.Update(existingData);
             }
 
-            db.Delete<TranslationProposal>(new Sql().Where("id=@tag1 and languageId=@tag2", new
+            db.Delete<TranslationProposal>(new Sql().Where("id=@tag1 AND languageId=@tag2", new
             {
                 tag1 = proposal.UniqueId,
                 tag2 = proposal.LanguageId
+            }));
+        }
+
+        [HttpPost]
+        public void RejectProposal(int id)
+        {
+            var db = DatabaseContext.Database;
+            db.Delete<TranslationProposal>(new Sql().Where("pk=@tag", new
+            {
+                tag = id
             }));
         }
     }
