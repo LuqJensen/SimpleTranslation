86af4baef887469aa15ccfb1a3dbb30e3400aabf
LuqJensen <lucas__jensen@hotmail.com>
2017-02-22 03:23:21 +0100

    Implemented listing and deleting translation tasks, U02...
    TODO: add functionality for adding a proposal to a task.
    Add the most recent proposal to the view.
    Add this users most recent proposal the view.
    Alter view based on user role (authorization)

---
+++ Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/Controllers/Api/TasksController.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http;
+using Struct.Umbraco.SimpleTranslation.WebTest.App_Plugins.SimpleTranslation.Models;
+using Umbraco.Core;
+using Umbraco.Core.Persistence;
+using Umbraco.Core.Models;
+using Umbraco.Web.WebApi;
+
+namespace Struct.Umbraco.SimpleTranslation.WebTest.App_Plugins.SimpleTranslation.Controllers.Api
+{
+    public class TasksController : UmbracoAuthorizedApiController
+    {
+        [HttpGet]
+        public object GetTranslationTasks()
+        {
+            var db = DatabaseContext.Database;
+            var tasks = db.Fetch<TranslationTask>(new Sql()
+                    .Select("t.*, l.languageCultureName AS language, d.[key] AS [key]")
+                    .From("dbo.simpleTranslationTasks t LEFT OUTER JOIN dbo.umbracoLanguage l ON t.languageId=l.id LEFT OUTER JOIN dbo.cmsDictionary d ON t.id=d.id"))
+                .ToList();
+
+            return tasks;
+        }
+
+        [HttpPost]
+        public void DeleteTask(int id)
+        {
+            var db = DatabaseContext.Database;
+            db.Delete<TranslationTask>(new Sql().Where("pk=@tag", new
+            {
+                tag = id
+            }));
+        }
+    }
+}
--- Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/Controllers/tasks.js
+++ Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/Controllers/tasks.js
@@ -1,5 +1,28 @@
-﻿angular.module("umbraco").controller("SimpleTranslation.Tasks.Controller", function () {
+﻿angular.module("umbraco").controller("SimpleTranslation.Tasks.Controller", function($scope, $http, $timeout) {
+    function getTranslationTasks() {
+        // Use jquery.post and jquery.get over Angular's http.post and http.get as these will wait for Angular to receive focus after dialogs.
+        $.get('/umbraco/backoffice/api/Tasks/GetTranslationTasks').success(function(response) {
+            // Instruct Angular to execute this on next digest. Workaround for Angular not updating regularly when not having focus due to dialogs.
+            $timeout(function() {
+                $scope.data = response;
+            });
+        });
+    }
 
-    
+    getTranslationTasks();
 
+    $scope.deleteTask = function(pk) {
+        event.preventDefault();
+
+        bootbox.confirm("Are you sure you want to delete this task?", function(result) {
+            if (result) {
+                // Use jquery.post and jquery.get over Angular's http.post and http.get as these will wait for Angular to receive focus after dialogs.
+                $.post("/umbraco/backoffice/api/Tasks/DeleteTask?id=" + pk).success(function() {
+                    getTranslationTasks();
+                }) /*.error(function(response) {
+                    console.log("response is void atm. TODO: error handling?");
+                })*/;
+            }
+        });
+    }
 });
