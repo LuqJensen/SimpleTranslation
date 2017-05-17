1bb782394e6857d7de7bdbd83d642cb1f2f3db22
RasiahT <rasiahmt@gmail.com>
2017-05-17 03:08:51 +0200

    Choose import as proposals or tasks

--- Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/BackOffice/SimpleTranslation/js/taskImportExport.js
+++ Struct.Umbraco.SimpleTranslation.WebTest/App_Plugins/SimpleTranslation/BackOffice/SimpleTranslation/js/taskImportExport.js
@@ -6,17 +6,39 @@ app.controller("SimpleTranslation.ImportExport.Controller", function($scope, $ht
 
     $scope.uploadXml = function () {
         event.preventDefault();
+        showMessage("Importing...", "waiting");
+        if ($("#file")[0].files.length === 0) {
+            showMessage("Please upload a file", "error");
+            return;
+        }
 
         var r = new FileReader();
         // Listen for read done event.
         r.onloadend = function (e) {
-            $http.post("/umbraco/backoffice/api/Tasks/ImportTranslationsFromXml", { value: e.target.result }).success(function (response) {
-                $scope.uploadError = "Xml file succesfully uploaded.";
-            }).error(function (response) { $scope.uploadError = response.ExceptionMessage; });
+            if ($scope.importAs === "Proposals") {
+                $http.post("/umbraco/backoffice/api/Tasks/ImportProposalsFromXml", { value: e.target.result }).success(function(response) {
+                    showMessage("Xml file succesfully imported.", "succes");
+                }).error(function (response) {
+                    showMessage(response.ExceptionMessage, "error");
+                });
+            } else if ($scope.importAs === "Translations"){
+                $http.post("/umbraco/backoffice/api/Tasks/ImportTranslationsFromXml", { value: e.target.result }).success(function (response) {
+                    showMessage("Xml file succesfully imported.", "succes");
+                }).error(function(response) {
+                    showMessage(response.ExceptionMessage, "error");
+                });
+            } else {
+                showMessage("Choose to import as Proposals or Translations", "error");
+            }
         };
 
         var f = $("#file")[0].files[0];
         // Begin reading
         r.readAsText(f);
+    }
+
+    function showMessage(message, type) {
+        $scope.message = message;
+        $scope.messageStyle = type;
     }
 });
